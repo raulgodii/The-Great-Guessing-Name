399768290cef10d8f56128608a9bb064a22aac33
raulgodii <raulgodii13@gmail.com>
2023-10-25 09:33:26 +0200

    colorsRandom

--- script.js
+++ script.js
@@ -1,7 +1,58 @@
 // main
 window.onload = () => {
-    easyMode = document.getElementById("easyMode");
-    hardMode = document.getElementById("hardMode");
+    easyModeEvent = document.getElementById("easyMode");
+    hardModeEvent = document.getElementById("hardMode");
 
+    // Solution
+    solution = document.getElementById("solution");
+    solution.innerHTML = generateRandomColor();
+
+    // Default Level
+    easyMode = true;
+    hardMode = false;
+
+    easyModeEvent.addEventListener("click", (e)=>{
+        easyMode = true;
+        hardMode = false;
+        easyModeEvent.style = "background-color: blue;"
+        hardModeEvent.style = "background-color: white;"
+    });
+
+    hardModeEvent.addEventListener("click", (e)=>{
+        easyMode = false;
+        hardMode = true;
+        easyModeEvent.style = "background-color: white;"
+        hardModeEvent.style = "background-color: blue;"
+    });
+
+    // Paint Divs
+    paintDivs(easyMode, hardMode);
+
+}
+
+function modeEvent(e){
     
+}
+
+function generateRandomColor(){
+    res = "RGB(";
+
+    for(let i=0; i<3; i++){
+        res += (Math.floor(Math.random()*256));
+        if(i!=2) res += ", ";
+    }
+
+    res += ")";
+
+    return res;
+}
+
+function paintDivs(easyMode, hardMode){
+    if(easyMode) n=3; else n=6;
+
+    color = document.getElementsByClassName("color");
+    console.log(color);
+    for(let i=0; i<n; i++){
+        color[i].style = ("background-color: " + generateRandomColor() + ";");
+    }
 }
